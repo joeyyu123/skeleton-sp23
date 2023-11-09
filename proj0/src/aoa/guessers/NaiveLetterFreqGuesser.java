255dd3899ec7d8c7947650b199ac687c8c14070b
joey <joeyyu159357789@gmail.com>
2023-11-09 17:41:13 +0800

    finished NaiveLetterFreqGuesser

--- proj0/src/aoa/guessers/NaiveLetterFreqGuesser.java
+++ proj0/src/aoa/guessers/NaiveLetterFreqGuesser.java
@@ -1,6 +1,8 @@
 package aoa.guessers;
 
 import aoa.utils.FileUtils;
+import java.util.*;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,14 +23,51 @@ public class NaiveLetterFreqGuesser implements Guesser {
      *  This task is similar to something you did in hw0b! */
     public Map<Character, Integer> getFrequencyMap() {
         // TODO: Fill in this method.
-        return null;
+
+        Map<Character, Integer> freqMap = new HashMap<>();
+
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                if (!freqMap.containsKey(c)) {
+                    freqMap.put(c, 1);
+                } else {
+                    freqMap.put(c, freqMap.get(c) + 1);
+                }
+            }
+        }
+        return freqMap;
     }
+
 
     /** Returns the most common letter in WORDS that has not yet been guessed
      *  (and therefore isn't present in GUESSES). */
     public char getGuess(List<Character> guesses) {
         // TODO: Fill in this method.
-        return '?';
+
+        TreeMap<Character, Integer> sortedMap = new TreeMap<>(getFrequencyMap());
+
+        for (Character guess : guesses) {
+            sortedMap.remove(guess);
+        }
+
+        int maxFreq = 0;
+        char maxChar = '?';
+
+        for (Map.Entry<Character, Integer> entry : sortedMap.entrySet()) {
+            if (!guesses.contains(entry.getKey())) {
+                if (entry.getValue() > maxFreq) {
+                    maxFreq = entry.getValue();
+                    maxChar = entry.getKey();
+                }
+            }
+        }
+
+        if (sortedMap.isEmpty()) {
+            return '?';
+        }
+
+        return maxChar;
+
     }
 
     public static void main(String[] args) {
@@ -36,7 +75,7 @@ public class NaiveLetterFreqGuesser implements Guesser {
         System.out.println("list of words: " + nlfg.words);
         System.out.println("frequency map: " + nlfg.getFrequencyMap());
 
-        List<Character> guesses = List.of('e', 'l');
+        List<Character> guesses = List.of('e', 'l','o');
         System.out.println("guess: " + nlfg.getGuess(guesses));
     }
 }
